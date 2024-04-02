8c02994c3d9a7dada5012dd663793d02501ed2b3
TopDev0101 <topdev0101@gmail.com>
2024-04-02 13:31:55 +0300

    add: added social icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -14,19 +14,30 @@ export default function footer() {
             Our mission is to empower individuals and businesses by providing
             them with the financial resources they need to achieve their goals.
           </div>
+          <div className="icon-group">
+            <img src="../../../static/images/facebook.png"></img>
+            <img src="../../../static/images/whatsapp.png"></img>
+            <img src="../../../static/images/linkedin.png"></img>
+            <img src="../../../static/images/instagram.png"></img>
+          </div>
         </div>
         <div className="footer-right">
           <div className="contact-us">CONTACT US</div>
           <div className="icon-wrap">
+            <img src="../../../static/images/phone.png"></img>
             <span>+91 99999 99999</span>
           </div>
           <div className="icon-wrap">
+            <img src="../../../static/images/mail.png"></img>
             <span>xyzfh5@gmail.com</span>
           </div>
-          <div className="icon-wrap">
-            <div>Address line-1</div>
-            <div>Address line-2</div>
-            <div>city, state-pin code.</div>
+          <div className="icon-wrap location-container">
+            <img src="../../../static/images/location.png"></img>
+            <div>
+              <div>Address line-1</div>
+              <div>Address line-2</div>
+              <div>city, state-pin code.</div>
+            </div>
           </div>
         </div>
       </div>
